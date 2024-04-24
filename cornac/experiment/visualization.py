d16c02c9282f2fe56eb83934bf64c3d48a9707ad
Ying <171607829@qq.com>
2024-04-24 14:53:43 +0200

    add plot_cost and allow specify bar/line chart

--- cornac/experiment/visualization.py
+++ cornac/experiment/visualization.py
@@ -13,11 +13,12 @@ class Visualization(object):
         """
         pass
                 
-    def visualize_experiemnt_result(self, EE):
+    def visualize_experiemnt_result(self, EE, kind = "bar"):
         """Visualize the result of the runing experiment
 
         Args:
             EE (_class_): the class Object of the Explainers_Experiment
+            kind (_string_): the type of the chart to be plotted, default is "bar", options: "bar", "line";
         """
         if EE.result is None:
             EE.run()
@@ -34,23 +35,39 @@ class Visualization(object):
         cost_data = data_df.iloc[:,-2:]
         print("=======Start Visualization=======")
         if len(metrics_data.columns)>0:
-            self._plot_exp_vs_metric(metrics_data)
+            self._plot_exp_vs_metric(metrics_data, kind)
         if len(cost_data.columns)>0:
             self._plot_cost(cost_data)
         print("=======Visualization Done=======")
         
-    def _plot_exp_vs_metric(self, df):
+    def _plot_exp_vs_metric(self, df, kind = "bar"):
         """Plot the bar chart: compare the performance of model/explainer pairs on a specific metric
 
         Args:
             df (_dataframe_): a dataframe with the model/explainer pairs as rows and the metrics as columns
+            kind (_string_): the type of the chart to be plotted, default is "bar", options: "bar", "line";
         """
         
-        print("Plot the bar chart for the metrics:")
-        for column in df.columns:
-            plt.bar(df.index, df[column], label=column)
-            plt.ylabel(column)
+        print(f"Plot the {kind} chart for the metrics:")
+        if kind == "bar":
+            plt.figure(figsize=(6, 4))
+            df.plot(kind=kind, ax=plt.gca())
+            plt.xticks(rotation=0)
             plt.show()
+        print(df)
+        if kind == "line":
+            plt.figure(figsize=(6, 4))
+            ax = df.plot(kind=kind, marker='o', linestyle='-')
+            # Set x-axis labels to be the DataFrame index
+            ax.set_xticks(range(len(df.index)))
+            ax.set_xticklabels(df.index)
+            plt.xticks(rotation=0)
+            plt.show()
+        
+        # for column in df.columns:
+        #     plt.bar(df.index, df[column], label=column)
+        #     plt.ylabel(column)
+        #     plt.show()
 
     def _plot_cost(self, df):
         """Plot the bar chart: compare the cost of training and evaluating the model/explainer pairs
@@ -59,4 +76,13 @@ class Visualization(object):
             df (_dataframe_): a dataframe with the model/explainer pairs as rows and the cost as columns
         """
         print("Plot the bar chart for the cost:")
-        pass
+        # Plotting the stacked bar chart
+        # plt.bar(df.index, df.iloc[:, -2], label=df.columns[-2])
+        # plt.bar(df.index, df.iloc[:, -1], bottom=df.iloc[:, -2], label=df.columns[-1])
+        df.plot(kind='bar', stacked=True)
+        # Adding labels and title
+        plt.xticks(rotation=0)
+        #plt.xlabel("model:explainer")
+        plt.ylabel("time(s)")
+        plt.legend()
+        plt.show()
